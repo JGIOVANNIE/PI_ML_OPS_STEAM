11f89a14351ef86da77fe962f845cfdb73b72796
JGIOVANNIE <jg.esquivel@outlook.com>
2024-01-01 21:55:17 -0600

    Creacion archivo main.py y actulizacion README.md

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# PI_ML_OPS_STEAM
-este proyecto trabajamos sobre datasets de la plataforma STEAM que es una plataforma de videojuegos, deployando una API para obtener consultas y realizando un modelo de Machine Learning 
+# _PI_ML_OPS_STEAM_
+
+
+<img src="https://th.bing.com/th?id=OIP.YoXkq3iMr1JApC1yonem4wHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2#include" >
+
+este proyecto trabajamos sobre datasets de la plataforma STEAM que es una plataforma de videojuegos, relizando el trabajo de un Data Engineer disponibilzando los dato asi como deployando una API para obtener consultas y realizando un modelo de Machine Learning.
+***
+<div style="text-align:center;">
+    <img src="https://th.bing.com/th/id/OIP.rHVv1cZ1gg3C62zqEuYCUwHaFf?w=234&h=180&c=7&r=0&o=5&pid=1.7" >
+</div>
+
+Las consultas que revisremos en este proyecto son las siguientes:
+
+* def PlayTimeGenre( genero : str ): Debe devolver año con mas horas jugadas para dicho género.
+
+* def UserForGenre( genero : str ): Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
+
+* *def UsersRecommend( año : int )*: Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado. (reviews.recommend = True y comentarios positivos/neutrales)
+
+* def *UsersNotRecommend( año : int )*: Devuelve el top 3 de juegos MENOS recomendados por usuarios para el año dado. (reviews.recommend = False y comentarios negativos)
+
+* *def sentiment_analysis( año : int )*: Según el año de lanzamiento, se devuelve una lista con la cantidad de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento.
+
+
+el sistema de recomndacion user_item:
+* *def* *recomendacion_usuario( id de usuario )*: Ingresando el id de un usuario, deberíamos recibir una lista con 5 juegos recomendados para dicho usuario.
+
+***
+
+dentro de este repositorio se encuentran todos los archivos utilizados en este proyecto:
+* *carpeta API:*
+    se enuentran los Dataset reducidos para el consumo en el deploy de la API
+* *carpeta Datasets:*
+    se encuentran los datasets crudos y los que ya fueron procesados para realizar nuestro ETL Y EDA
+* *carpeta Notebooks:* 
+    se encuentran los notebooks de cada parte de el proceso: EDA,ETL,ModeloML,Pruebas API
+* *Diccionario de Datos Steam* es un diccionario para entender mejor cada una de las columnas en loos Datasets
+* *main.py* es el archivo donde se ejecuta nuestra API
+***
+
+
+
+LinkedIn: www.linkedin.com/in/joshua-giovanni-esquivel-fuentes-06987a190
+
+*******    
---
+++ main.py
@@ -0,0 +1,167 @@
+# relizaremos el deploy de nuestras consultas 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+from typing import List, Dict, Union
+import pandas as pd
+import pyarrow
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import PCA
+import numpy as np 
+
+
+
+df_PTG = pd.read_parquet('PI_ML_OPS_STEAM/API/PlayTimeGenre.parquet')
+df_UFG = pd.read_parquet('PI_ML_OPS_STEAM/API/UserForGenre.parquet')
+df_UsersR = pd.read_parquet('PI_ML_OPS_STEAM/API/UsersR.parquet')
+
+
+@app.get("/play_time_genre/{genero}")
+async def play_time_genre(genero: str) -> Dict[str, int]:
+    # Filtrar los datos por género
+        df_genero = df_PTG[df_PTG['genres'].str.lower().str.contains(genero.lower())]
+
+        if df_genero.empty:
+            return {"mensaje": f"No hay datos para el género: {genero}"}
+
+    # Convertir la columna playtime_forever de minutos a horas
+        df_genero['playtime_forever'] = (df_genero['playtime_forever'] / 60).round(2)
+
+    # Agrupar los datos por año y sumar las horas jugadas
+        df_agrupado = df_genero.groupby('año')['playtime_forever'].sum().reset_index()
+
+    # Encontrar el año con más horas jugadas
+        max_playtime_year = df_agrupado.loc[df_agrupado['playtime_forever'].idxmax(), 'año']
+
+        return {
+        f"Año de lanzamiento con más horas jugadas para Género {genero}": max_playtime_year
+        }
+
+@app.get("/user_for_genre/{genero}")
+async def user_for_genre(genero: str) -> Dict[str, Union[str, List[Dict[str, int]]]]:
+        # Filtrar los datos por género
+        df_genero = df_UFG[df_UFG['genres'].str.lower().str.contains(genero.lower(), na=False)]
+
+        if df_genero.empty:
+            return {"mensaje": f"No hay datos para el género: {genero}"}
+
+        # Convertir la columna playtime_forever de minutos a horas
+        df_genero['playtime_forever'] = (df_genero['playtime_forever'] / 60).round(2)
+
+        # Agrupar los datos por usuario y año
+        df_agrupado = df_genero.groupby(['user_id', 'año']).agg({'playtime_forever': 'sum'}).reset_index()
+
+        # Encontrar el usuario con más horas jugadas
+        max_playtime_user = df_agrupado.loc[df_agrupado['playtime_forever'].idxmax(), 'user_id']
+
+        # Crear una lista de la acumulación de horas jugadas por año para ese usuario
+        user_data = df_agrupado[df_agrupado['user_id'] == max_playtime_user]
+        user_data = user_data.rename(columns={'playtime_forever': 'Horas'})
+        horas_jugadas = user_data[['año', 'Horas']].to_dict('records')
+
+        return {
+            "Usuario con más horas jugadas para el género proporcionado es": max_playtime_user,
+            "Horas jugadas": horas_jugadas
+        }
+
+@app.get("/users_recommend/{año}")
+async def users_recommend(año: int) -> List[Dict[str, str]]:
+        # Filtrar las reseñas que son recomendaciones y tienen un análisis de sentimiento positivo o neutral
+        filtro = df_UsersR[(df_UsersR['recommend'] == True) & (df_UsersR['sentiment_analysis'].isin([1, 2]))]
+
+        # Filtrar los juegos que se lanzaron en el año dado
+        filtro = filtro[filtro['año'] == año]
+
+        # Contar las recomendaciones para cada juego
+        cuenta_de_recomendaciones = filtro['title'].value_counts()
+
+        # Obtener los tres juegos con más recomendaciones, si hay al menos tres juegos
+        if len(cuenta_de_recomendaciones) >= 3:
+            top_juegos = cuenta_de_recomendaciones.nlargest(3).index.tolist()
+            return [
+                {"Puesto 1": top_juegos[0]},
+                {"Puesto 2": top_juegos[1]},
+                {"Puesto 3": top_juegos[2]}
+            ]
+        else:
+            return [{"mensaje": "No hay suficientes juegos con recomendaciones para el año dado."}]
+
+
+@app.get("/users_not_recommend/{año}")
+async def users_not_recommend(año: int) -> List[Dict[str, str]]:
+        # Filtrar las reseñas que no son recomendaciones y tienen un análisis de sentimiento negativo
+        filtrado = df_UsersR[(df_UsersR['recommend'] == False) & (df_UsersR['sentiment_analysis'].isin([0]))]
+
+        # Filtrar los juegos que se lanzaron en el año dado
+        filtrado = filtrado[filtrado['año'] == año]
+
+        # Contar las no recomendaciones para cada juego
+        cuenta_negativa = filtrado['title'].value_counts()
+
+        # Devolver los tres juegos con más no recomendaciones
+        top_juegos_norecomendados = cuenta_negativa.nlargest(3).index.tolist()
+
+        return [
+            {"Puesto 1": top_juegos_norecomendados[0]},
+            {"Puesto 2": top_juegos_norecomendados[1]},
+            {"Puesto 3": top_juegos_norecomendados[2]}
+        ]
+
+@app.get("/sentiment_analysis/{año}")
+async def sentiment_analysis(año: int) -> Dict[str, int]:
+        # Filtramos el dataframe por año
+        df_filtrado = df_UsersR[df_UsersR['año'] == año]
+
+        # Contar la cantidad de registros de reseñas que se encuentren categorizados con un análisis de sentimiento
+        contar_sentimientos = df_filtrado['sentiment_analysis'].value_counts()
+
+        # Crear un nuevo diccionario con el formato deseado
+        sentimientos = {
+            f"En el año {año} hubo comentarios": {
+                "positivos": contar_sentimientos.get(2, 0),
+                "neutrales": contar_sentimientos.get(1, 0),
+                "negativos": contar_sentimientos.get(0, 0)
+            }
+        }
+
+        return sentimientos
+
+
+# REALIZAMOS EL MODELO DE MACHINE LEARNING
+ 
+
+
+@app.get("/recomendacion_usuario/{id_usuario}")
+async def recomendacion_usuario(id_usuario: int) -> dict:
+    # Creamos una matriz de usuarios x juegos con recomendaciones
+    matriz_user_item = df_UsersR.pivot_table(index='user_id', columns='title', values='recommend', fill_value=0)
+    # Calculamos la  similitud del coseno entre usuarios
+    # Reducir la dimensión con PCA
+    pca = PCA(n_components=1, svd_solver='full')  # Ajusta el número de componentes 
+    matriz_user_item_reducida = pca.fit_transform(matriz_user_item)
+
+
+    # Explorar la varianza explicada
+    varianza_explicada = pca.explained_variance_ratio_
+    umbral_varianza = 0.95 # retenemos n cantidad de varianza para nuestro modelo
+    numero_componentes = sum(np.cumsum(varianza_explicada) <= umbral_varianza) + 1
+
+    # Calcular similitud del coseno entre usuarios en el espacio reducido
+    similitud_user_item = cosine_similarity(matriz_user_item_reducida)   
+
+    # Creamos un DataFrame de similitudes entre usuarios
+    df_similares_user_item = pd.DataFrame(similitud_user_item, index=matriz_user_item.index, columns=matriz_user_item.index)
+    # Obtener las similitudes del usuario desde el dataframe df_similares_user_item
+    usuario_similaridades = df_similares_user_item.loc[id_usuario]
+
+    # Obtener los juegos recomendados para el usuario
+    juegos_recomendados = matriz_user_item.columns[np.where(matriz_user_item.loc[id_usuario] == 0)][:5]
+
+    # Crear un diccionario con el formato deseado
+    resultado_recomendacion = {
+        "mensaje": "Las recomendaciones de este usuario son:",
+        "juegos_recomendados": list(juegos_recomendados)
+    }
+
+    return resultado_recomendacion        
