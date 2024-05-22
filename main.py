a43d60b1148b84db6bceb27217b65e3c255256c1
JGIOVANNIE <jg.esquivel@outlook.com>
2024-05-22 11:27:26 -0600

    v 1.4

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pyarrow
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import PCA
 import numpy as np 
+import joblib
 
 app = FastAPI(title='STEAM Games', description='Esta es una aplicación para realizar consultas sobre todo el mundo de STEAM.')
 
@@ -13,6 +14,7 @@ app = FastAPI(title='STEAM Games', description='Esta es una aplicación para rea
 df_PTG = pd.read_parquet('./API/PlayTimeGenre.parquet')
 df_UFG = pd.read_parquet('./API/UserForGenre.parquet')
 df_UsersR = pd.read_parquet('./API/UsersR.parquet')
+modelo = pd.read_parquet('./API/modelo.parquet')
 
 
 @app.get("/play_time_genre/{genero}")
@@ -39,13 +41,10 @@ async def play_time_genre(genero: str) -> Dict[str, int]:
 @app.get("/user_for_genre/{genero}")
 async def user_for_genre(genero: str) -> Dict[str, Union[str, List[Dict[str, float]]]]:
         # Filtrar los datos por género
-        df_genero = df_UFG[df_UFG['genres'].str.lower().str.contains(genero.lower(), na=False)]
+        df_genero = df_UFG[df_UFG['genres'].str.contains(genero.lower(), na=False)]
 
         if df_genero.empty:
             return {"mensaje": f"No hay datos para el género: {genero}"}
-
-        # Convertir la columna playtime_forever de minutos a horas
-        df_genero.loc[:, 'playtime_forever'] = (df_genero['playtime_forever'] / 60).round(2)
 
         # Agrupar los datos por usuario y año
         df_agrupado = df_genero.groupby(['user_id', 'año']).agg({'playtime_forever': 'sum'}).reset_index()
@@ -69,7 +68,7 @@ async def users_recommend(anio: int) -> List[Dict[str, str]]:
         filtro = df_UsersR[(df_UsersR['recommend'] == True) & (df_UsersR['sentiment_analysis'].isin([1, 2]))]
 
         # Filtrar los juegos que se lanzaron en el año dado
-        filtro = filtro[filtro['año'] == anio]
+        filtro = filtro[filtro['anio'] == anio]
 
         # Contar las recomendaciones para cada juego
         cuenta_de_recomendaciones = filtro['title'].value_counts()
@@ -92,7 +91,7 @@ async def users_not_recommend(anio: int) -> List[Dict[str, str]]:
         filtrado = df_UsersR[(df_UsersR['recommend'] == False) & (df_UsersR['sentiment_analysis'].isin([0]))]
 
         # Filtrar los juegos que se lanzaron en el año dado
-        filtrado = filtrado[filtrado['año'] == anio]
+        filtrado = filtrado[filtrado['anio'] == anio]
 
         # Contar las no recomendaciones para cada juego
         cuenta_negativa = filtrado['title'].value_counts()
@@ -109,16 +108,10 @@ async def users_not_recommend(anio: int) -> List[Dict[str, str]]:
         else:
             return [{"error": "No hay suficientes juegos no recomendados para el año dado."}]
 
-        return [
-            {"Puesto 1": top_juegos_norecomendados[0]},
-            {"Puesto 2": top_juegos_norecomendados[1]},
-            {"Puesto 3": top_juegos_norecomendados[2]}
-        ]
-
 @app.get('/sentiment_analysis/{anio}')
 async def sentiment_analysis(anio: int) -> Dict[str, Dict[str, int]]:
         # Filtramos el dataframe por año
-        df_filtrado = df_UsersR[df_UsersR['año'] == anio]
+        df_filtrado = df_UsersR[df_UsersR['anio'] == anio]
 
         # Contar la cantidad de registros de reseñas que se encuentren categorizados con un análisis de sentimiento
         contar_sentimientos = df_filtrado['sentiment_analysis'].value_counts()
@@ -133,43 +126,19 @@ async def sentiment_analysis(anio: int) -> Dict[str, Dict[str, int]]:
         }
 
         return sentimientos
-
-
-# REALIZAMOS EL MODELO DE MACHINE LEARNING
  
-
-
-app.get("/recomendacion_usuario/{id_usuario}")
+@app.get("/recomendacion usuario/{id_usuario}")
 async def recomendacion_usuario(id_usuario: str) -> dict:
-    # Creamos una matriz de usuarios x juegos con recomendaciones
-    matriz_user_item = df_UsersR.pivot_table(index='user_id', columns='title', values='recommend', fill_value=0)
-    # Calculamos la  similitud del coseno entre usuarios
-    # Reducir la dimensión con PCA
-    pca = PCA(n_components=1, svd_solver='full')  # Ajusta el número de componentes 
-    matriz_user_item_reducida = pca.fit_transform(matriz_user_item)
-
-
-    # Explorar la varianza explicada
-    varianza_explicada = pca.explained_variance_ratio_
-    umbral_varianza = 0.95 # retenemos n cantidad de varianza para nuestro modelo
-    numero_componentes = sum(np.cumsum(varianza_explicada) <= umbral_varianza) + 1
-
-    # Calcular similitud del coseno entre usuarios en el espacio reducido
-    similitud_user_item = cosine_similarity(matriz_user_item_reducida, numero_componentes)   
-
-    # Creamos un DataFrame de similitudes entre usuarios
-    df_similares_user_item = pd.DataFrame(similitud_user_item, index=matriz_user_item.index, columns=matriz_user_item.index)
-    # Obtener las similitudes del usuario desde el dataframe df_similares_user_item
-    usuario_similaridades = df_similares_user_item.loc[id_usuario]
-
     # Obtener los juegos recomendados para el usuario
-    juegos_recomendados = matriz_user_item.columns[np.where(matriz_user_item.loc[id_usuario] == 0)][:5]
-
+    juegos_recomendados = modelo.columns[np.where(modelo.loc[id_usuario] == 0)][:5]
     # Crear un diccionario con el formato deseado
     resultado_recomendacion = {
-        "Las recomendaciones de este usuario son:": list(juegos_recomendados)
+        "Las recomendaciones el usuario son:": list(juegos_recomendados)
     }
 
     return resultado_recomendacion        
   
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
